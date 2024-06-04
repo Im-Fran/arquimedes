260721b74bf4edafd48107a74602375d4a07be61
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-06-03 23:19:49 -0400

    patch: se mueven iconos a su carpeta y se elimina barra de scroll

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => <>
             <img
                 src={"/logo.png"}
                 alt={"Arquimedes"}
-                className={"w-24"}
+                className={"w-20"}
             />
 
             <div>
@@ -15,20 +15,20 @@ const App = () => <>
                 </button>
             </div>
         </nav>
-        <div className={"border border-b border-[6px] border-black w-full mb-10"}></div>
+        <div className={"border border-b border-[6px] border-black w-full my-2.5"}></div>
     </div>
-    <div className={"container mx-auto"}>
+    <div className={"container mx-auto min-h-screen"}>
         <div className={"flex justify-center my-5"}>
             <img
                 src={"/banner.png"}
                 alt={"Persona entregando medicamentos"}
-                className={"w-full h-96 object-cover object-center rounded-[5vw] shadow-lg"}
+                className={"w-full h-80 object-cover object-center rounded-2xl shadow-lg"}
             />
         </div>
 
         <div className={"grid grid-cols-4 gap-5"}>
             {botones.map(boton => (<button key={boton} className={"col-span-1 flex flex-col items-center justify-center bg-[#5271FF] rounded-lg p-5 hover:shadow-2xl transition duration-500 ease-in-out"}>
-                <img src={`/${boton}.png`} alt={boton} className={"h-16"}/>
+                <img src={`/iconos/${boton}.png`} alt={boton} className={"h-16"}/>
                 <span className={"text-zinc-100 uppercase mt-5 font-bold"}>{boton}</span>
             </button>))}
         </div>
